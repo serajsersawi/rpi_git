8b917660239173d49fa984ac3fb75e6d86c29e78
Seraj Alsersawi <serajsersawi98@gmail.com>
2024-02-25 10:25:36 +0000

    Create i2c_device.c

---
+++ i2c_device.c
@@ -0,0 +1,14 @@
+#include"I2CDevice.h"
+#include<iostream>
+#include<sstream>
+#include<fcntl.h>
+#include<stdio.h>
+#include<iomanip>
+#include<unistd.h>
+#include<sys/ioctl.h>
+#include<linux/i2c.h>
+#include<linux/i2c-dev.h>
+using namespace std;
+
+#define HEX(x) setw(2) << setfill('0') << hex << (int)(x)
+
