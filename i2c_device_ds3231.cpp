9d7340f3a1b20b1b29d01a612341c26380661a36
Seraj Alsersawi <serajsersawi98@gmail.com>
2024-02-27 15:58:58 +0000

    Initial test
    Initial test of updating registers

---
+++ i2c_device_ds3231.cpp
@@ -0,0 +1,132 @@
+/*
+ * ADXL345.cpp  Created on: 17 May 2014
+ * Copyright (c) 2014 Derek Molloy (www.derekmolloy.ie)
+ * Made available for the book "Exploring Raspberry Pi"
+ * See: www.exploringrpi.com
+ * Licensed under the EUPL V.1.1
+ *
+ * This Software is provided to You under the terms of the European 
+ * Union Public License (the "EUPL") version 1.1 as published by the 
+ * European Union. Any use of this Software, other than as authorized 
+ * under this License is strictly prohibited (to the extent such use 
+ * is covered by a right of the copyright holder of this Software).
+ * 
+ * This Software is provided under the License on an "AS IS" basis and 
+ * without warranties of any kind concerning the Software, including 
+ * without limitation merchantability, fitness for a particular purpose, 
+ * absence of defects or errors, accuracy, and non-infringement of 
+ * intellectual property rights other than copyright. This disclaimer 
+ * of warranty is an essential part of the License and a condition for 
+ * the grant of any rights to this Software.
+ * 
+ * For more details, see http://www.derekmolloy.ie/
+ */
+
+#include "i2c_device_ds3231.h"
+#include <iostream>
+#include <unistd.h>
+#include <math.h>
+#include <stdio.h>
+
+using namespace std;
+
+namespace i2c {
+
+//From fig 1. of the DS3231 Data sheet (page 11)
+//Register address mapping
+//Since the addresses are sequential, an enum can also be used.
+#define SECONDS_REG        		0x00   //Device ID
+#define MINUTES_REG    	   		0x01   //Tap Threshold
+#define HOURS_REG          		0x02   //X-axis Offset
+#define DAY_REG            		0x03   //Y-axis Offset
+#define DATE_REG           		0x04   //Z-axis Offset
+#define MONTH_CENT_REG     		0x05   //Tap duration
+#define YEAR_REG           		0x06   //Tap latency
+#define ALARM1_SEC_REG     		0x07   //Tap window
+#define ALARM1_MIN_REG     		0x08   //Activity threshold
+#define ALARM1_HR_REG           0x09   //Threshold inactivity
+#define ALARM1_DAY_DATE_REG     0x0A   //Inactivity time
+#define ALARM2_MIN_REG     		0x0B   //Activity threshold
+#define ALARM2_HR_REG      		0x0C   //Threshold inactivity
+#define ALARM2_DAY_DATE_REG    	0x0D   //Inactivity time
+#define CTRL_REG           		0x0E   //Free-fall threshold
+#define CTRL_STAT_REG      		0x0F   //Free-fall time
+#define AGING_OFFSET_REG   		0x10   //Axis control for single tap/double tap
+#define TEMP_MSB_REG       		0x11   //Source of single tap/double tap
+#define TEMP_LSB_REG       		0x12   //Data rate and power mode control
+
+/**
+ * The constructor for the ADXL345 accelerometer object. It passes the bus number and the
+ * device address (with is 0x53 by default) to the constructor of I2CDevice. All of the states
+ * are initialized and the registers are updated.
+ * @param I2CBus The bus number that the ADXL345 device is on - typically 0 or 1
+ * @param I2CAddress The address of the ADLX345 device (default 0x53, but can be altered)
+ */
+ 
+/*Constructor to initialize the rtc module given the i2c bus and the address*/
+/*Initialize the module to the default values
+	Time and date:
+	H_:M_:S_  D_ M_ Y___ 
+	00:00:00  01/01/2000
+	
+	Control register set to -> (0x1C):
+	EOSC	BBSWQ	VONC	RS2		RS1		INTC	A2IE 	A1IE
+	1		0		0		0		1		0		1		0
+	
+	All other registers default to 0x00
+ 
+*/
+i2c_device_ds3231::i2c_device_ds3231(unsigned int I2CBus, unsigned int I2CAddress):
+	I2CDevice(I2CBus, I2CAddress){   // this member initialisation list calls the parent constructor
+	
+	this->I2CAddress = I2CAddress;
+	this->I2CBus = I2CBus;
+	
+	this->seconds = DS3231_REGISTER_SECONDS_DEFAULT;
+	this->minutes = DS3231_REGISTER_MINUTES_DEFAULT;
+	this->hours = DS3231_REGISTER_HOURS_DEFAULT;
+	this->day = DS3231_REGISTER_DAY_OF_WEEK_DEFAULT;
+	this->date = DS3231_REGISTER_DATE_DEFAULT;
+	this->month = DS3231_REGISTER_MONTH_DEFAULT;
+	this->year = DS3231_REGISTER_YEAR_DEFAULT;
+	
+	this->hr_mode = i2c_device_ds3231::TWENTYFOUR;
+	this->wave = i2c_device_ds3231::WAVE_2;
+	this->clk = i2c_device_ds3231::CLOCK_RUN;
+		
+	this->initUpdateAllRegisters();
+}
+
+//Update all registers (time and date)
+int i2c_device_ds3231::initUpdateAllRegisters(){
+
+   int check = 0;
+   
+   check += (this->writeRegister(SECONDS_REG, DS3231_REGISTER_SECONDS_DEFAULT)); //Seconds: 00
+   check += (this->writeRegister(MINUTES_REG, DS3231_REGISTER_MINUTES_DEFAULT)); //Minutes: 00
+   check += (this->writeRegister(HOURS_REG, DS3231_REGISTER_HOURS_DEFAULT)); //Hours: 00 | 24hr format
+   check += (this->writeRegister(DAY_REG, DS3231_REGISTER_DAY_OF_WEEK_DEFAULT)); //User defined 01 Monday
+   check += (this->writeRegister(DATE_REG, DS3231_REGISTER_DATE_DEFAULT)); //01
+   check += (this->writeRegister(YEAR_REG, DS3231_REGISTER_YEAR_DEFAULT)); //2000
+   
+   return check;
+}
+
+i2c_device_ds3231::~i2c_device_ds3231() {}
+
+/*********************************************************************************************/
+
+/**
+ * Useful debug method to display the pitch and roll values in degrees on a single standard output line
+ * @param iterations The number of 0.1s iterations to take place.
+ */
+void i2c_device_ds3231::displayTimeAndDate(){
+
+}
+
+void i2c_device_ds3231::displayTemperature(){
+
+}
+
+
+} /* namespace exploringRPi */
---
+++ i2c_device_ds3231.h
@@ -0,0 +1,149 @@
+/*
+ * ADXL345.h  Created on: 17 May 2014
+ * Copyright (c) 2014 Derek Molloy (www.derekmolloy.ie)
+ * Made available for the book "Exploring Raspberry Pi"
+ * See: www.exploringrpi.com
+ * Licensed under the EUPL V.1.1
+ *
+ * This Software is provided to You under the terms of the European
+ * Union Public License (the "EUPL") version 1.1 as published by the
+ * European Union. Any use of this Software, other than as authorized
+ * under this License is strictly prohibited (to the extent such use
+ * is covered by a right of the copyright holder of this Software).
+ *
+ * This Software is provided under the License on an "AS IS" basis and
+ * without warranties of any kind concerning the Software, including
+ * without limitation merchantability, fitness for a particular purpose,
+ * absence of defects or errors, accuracy, and non-infringement of
+ * intellectual property rights other than copyright. This disclaimer
+ * of warranty is an essential part of the License and a condition for
+ * the grant of any rights to this Software.
+ *
+ * For more details, see http://www.derekmolloy.ie/
+ */
+
+#ifndef I2C_DEVICE_DS3231_H_
+#define I2C_DEVICE_DS3231_H_
+#include"i2c_device.h"
+
+/// The ADXL345 has 0x40 registers (0x01 to 0x1C are reserved and should not be accessed)
+#define BUFFER_SIZE 0x40
+
+namespace i2c {
+
+/**
+ * @class ADXL345
+ * @brief Specific class for the ADXL345 Accelerometer that is a child of the I2CDevice class
+ * Protected inheritance means that the public I2CDevice methods are not publicly accessible
+ * by an object of the ADXL345 class.
+ */
+ 
+#define DS3231_REGISTER_SECONDS_DEFAULT                       0X00
+#define DS3231_REGISTER_MINUTES_DEFAULT                       0X00
+#define DS3231_REGISTER_HOURS_DEFAULT                         0X00
+#define DS3231_REGISTER_DAY_OF_WEEK_DEFAULT                   0X01
+#define DS3231_REGISTER_DATE_DEFAULT                          0X01
+#define DS3231_REGISTER_MONTH_DEFAULT                         0X01
+#define DS3231_REGISTER_YEAR_DEFAULT                          0X00
+#define DS3231_REGISTER_ALARM1_SECONDS_DEFAULT                0X00
+#define DS3231_REGISTER_ALARM1_MINUTES_DEFAULT                0X00            
+#define DS3231_REGISTER_ALARM1_HOURS_DEFAULT                  0X00
+#define DS3231_REGISTER_ALARM1_DAY_OF_WEEK_OR_DATE_DEFAULT    0X00
+#define DS3231_REGISTER_ALARM2_MINUTES_DEFAULT                0X00
+#define DS3231_REGISTER_ALARM2_HOURS_DEFAULT                  0X00    
+#define DS3231_REGISTER_ALARM2_DAY_OF_WEEK_OR_DATE_DEFAULT    0X00
+#define DS3231_REGISTER_CONTROL_DEFAULT                       0X1C
+#define DS3231_REGISTER_CONTROL_STATUS_DEFAULT                0X00
+#define DS3231_REGISTER_AGING_OFFSET_DEFAULT                  0X00
+
+class i2c_device_ds3231:protected i2c_device{
+
+public:
+	enum MAP {SECOND, MINUTE, HOUR, DAY_OF_WEEK, DATE, MONTH, YEAR, CONTROL, CONTROL_STATUS, AGING_OFFSET, ALARM1, ALARM2, ALARMS, TEMPERATURE, TIME, ALL};
+	enum SQR_WAVES {
+		WAVE_1, //1KHz
+		WAVE_2,	//1.024kHz
+		WAVE_3, //4.096kHz
+		WAVE_4	//8.192kHz 
+		};
+	enum RUNCLK_STATE{CLOCK_HALT, CLOCK_RUN};
+	/*This enum is redundant, the register map in the cpp file is used instead*/
+	enum DS3231_REG {
+		DS3231_REGISTER_SECONDS, 
+		DS3231_REGISTER_MINUTES, 
+		DS3231_REGISTER_HOURS, 
+		DS3231_REGISTER_DAY_OF_WEEK, 
+		DS3231_REGISTER_DATE, 
+		DS3231_REGISTER_MONTH, 
+		DS3231_REGISTER_YEAR, 
+		DS3231_REGISTER_ALARM1_SECONDS,
+		DS3231_REGISTER_ALARM1_MINUTES,
+		DS3231_REGISTER_ALARM1_HOURS,
+		DS3231_REGISTER_ALARM1_DAY_OF_WEEK_OR_DATE,
+		DS3231_REGISTER_ALARM2_MINUTES,
+		DS3231_REGISTER_ALARM2_HOURS,
+		DS3231_REGISTER_ALARM2_DAY_OF_WEEK_OR_DATE,
+		DS3231_REGISTER_CONTROL,
+		DS3231_REGISTER_CONTROL_STATUS,
+		DS3231_REGISTER_AGING_OFFSET,
+		DS3231_REGISTER_ALARM2_TEMP_MSB,
+		DS3231_REGISTER_ALARM2_TEMP_LSB
+	};
+	// bit 6 of the hours register
+	enum HOUR_MODE { 
+		TWENTYFOUR,
+		TWELVE
+	};
+	//User defined sequence for days of the week
+	//Could be changed but must be sequential
+	enum DAY_OF_WEEK { 
+		MONDAY = 1,
+		TUESDAY,
+		WEDNESDAY,
+		THURSDAY,
+		FRIDAY,
+		SATURDAY,
+		SUNDAY
+	};
+	
+
+
+private:
+	/*private function*/
+	unsigned int I2CBus, I2CAddress;
+	unsigned char *registers;
+	i2c_device_ds3231::AM_PM hr_mode;
+	i2c_device_ds3231::SQR_WAVES wave;
+	i2c_device_ds3231::RUNCLK_STATE clk;
+	
+	uint8_t seconds, minutes, hours, day, date, month, year; // raw 2's complement values
+	
+	uint8_t temperature;
+
+	static uint8_t register_current_value;        /*used to read current values of ds3231 registers*/
+	static uint8_t register_new_value;        /*used to write new values to ds3231 registers*/
+	virtual int updateAllRegisters();
+	virtual int resetAllRegisters();
+
+public:
+	/*public functions APIs*/
+	i2c_device_ds3231(unsigned int I2CBus, unsigned int I2CAddress=0x68);
+	virtual int i2c_device_ds3231::initUpdateAllRegisters();
+
+
+	
+	static int decimalToBCD(int decimalNumber) {
+        int tens = decimalNumber / 10;
+        int ones = decimalNumber % 10;
+        return (tens << 4) | ones;
+    }
+
+	// Debugging method to display and update the pitch/roll on the one line
+	virtual void i2c_device_ds3231::displayTimeAndDate();
+	virtual void i2c_device_ds3231::displayTemperature();
+	virtual ~i2c_device_ds3231();
+};
+
+} /* namespace i2c */
+
+#endif /* I2C_DEVICE_DS3231_H_ */
