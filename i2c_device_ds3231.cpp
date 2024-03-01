28814716b5f5700355997cc92239d50864e9e8f9
Seraj Alsersawi <serajsersawi98@gmail.com>
2024-03-01 01:39:42 +0000

    fixed minutes mask

--- i2c_device_ds3231.cpp
+++ i2c_device_ds3231.cpp
@@ -341,7 +341,7 @@ unsigned int i2c_device_ds3231::setMinutes(unsigned int minutes, unsigned int re
 	if(minutes < 60){
 		cout << " setting minutes " <<endl;
 		unsigned int registerVal = this->readRegister(targetRegister);
-		registerVal &=  ((~(0x9F)) | (decimalToBCD(minutes)));
+		registerVal &=  ((~(0x7F)) | (decimalToBCD(minutes)));
 		cout << " register val " << registerVal <<endl;
 		this->writeRegister(targetRegister, registerVal);
 		return 0;
