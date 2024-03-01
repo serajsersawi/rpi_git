01750dbccc3b9e33d9251ea2adc059d7de1aea80
Seraj Alsersawi <serajsersawi98@gmail.com>
2024-03-01 02:08:56 +0000

    hr 23

--- rtc_app.cpp
+++ rtc_app.cpp
@@ -74,7 +74,7 @@ int main() {
     */
 	
    sleep(2);
-   rtc.setAlarm1(i2c_device_ds3231::DAY_OF_MONTH, MS_MATCH, 1, 1, 13, 4, 0); 
+   rtc.setAlarm1(i2c_device_ds3231::DAY_OF_MONTH, MS_MATCH, 1, 1, 23, 4, 0); 
    
    
    return 0;
