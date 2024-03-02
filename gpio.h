183121be2057d689e502ec676828f63ebca80f3a
Seraj Alsersawi <serajsersawi98@gmail.com>
2024-03-02 11:12:02 +0000

    fixed header

--- gpio.h
+++ gpio.h
@@ -1,23 +1,21 @@
-
-
 #ifndef GPIO_H_
 #define GPIO_H_
 
-#include<string>
+#include <string>
 
-class LED{
-   private:                // the following is part of the implementation
-      string gpioPath;     // private states
-      int    gpioNumber;
-      void writeSysfs(string path, string filename, string value);
-   public:                 // part of the public interface
-      LED(int gpioNumber); // the constructor -- create the object
-      virtual void turnLEDOn();
-      virtual void turnLEDOff();
-	  virtual void toggleLED();
-      virtual void displayState();
-	  virtual unsigned int getState();
-      virtual ~LED();      // the destructor -- called automatically
+class LED {
+private:
+    std::string gpioPath; // Use std::string to avoid namespace pollution
+    int gpioNumber;
+    void writeSysfs(std::string path, std::string filename, std::string value);
+public:
+    LED(int gpioNumber); // Constructor
+    virtual void turnLEDOn();
+    virtual void turnLEDOff();
+    virtual void toggleLED();
+    virtual void displayState();
+    virtual unsigned int getState(); // Returns the state as an unsigned int
+    virtual ~LED(); // Destructor
 };
 
-#endif /* I2C_DEVICE_DS3231_H_ */
+#endif /* GPIO_H_ */
