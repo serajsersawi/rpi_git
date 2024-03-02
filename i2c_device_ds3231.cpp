34364f3100e65e8559e5f579438db9253c45869b
Seraj Alsersawi <serajsersawi98@gmail.com>
2024-03-02 01:42:35 +0000

    fixed enable disable

--- i2c_device_ds3231.cpp
+++ i2c_device_ds3231.cpp
@@ -358,15 +358,6 @@ void i2c_device_ds3231::stopSquareWave(){
 	this->writeRegister(CTRL_REG, (RegisterVal));
 }
 
-void i2c_device_ds3231::enableAlarm1(){
-	stopSquareWave();
-	clearAlarm1Flag();
-	unsigned int RegisterVal = this->readRegister(CTRL_REG);
-	//enable interrupt
-	RegisterVal |= (1 << 0);
-	RegisterVal |= (1 << 2);
-	this->writeRegister(CTRL_REG, (RegisterVal));
-}
 
  
 void i2c_device_ds3231::displayTimeAndDate(){
